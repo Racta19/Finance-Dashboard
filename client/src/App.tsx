d20ca9bc6e53ecb8d2125161f7ecc1330bb3ed67
Muhammad Rohail Omer <77564698+Racta19@users.noreply.github.com>
2023-08-02 10:57:53 +0500

    Navbar Implemented with partial functionality

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { useMemo } from 'react'
+import { Box, ThemeProvider, CssBaseline } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from './theme'
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
+
+import Navbar from '@/scenes/Navbar/Navbar'
+import Dashboard from './scenes/Dashboard/Dashboard'
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
+  return (
+      <div className='app'>
+        <BrowserRouter >
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Navbar />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Routes>
+                <Route path='/'  element={<Dashboard />}/>
+                <Route path='/predictions'  element={<div>Predictions Page</div>}/>
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
+
+  )
+}
+
+export default App
